7677d6f98bb4c582f1bba023f4f0092d0ce53bef
Cheyenne <cheyennejjohnson96@gmail.com>
2025-08-11 09:37:14 -0400

    adding logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
-function App() {
-  const [count, setCount] = useState(0)
+import Home from "./pages/Home";
+import Category from "./pages/Category";
+import RecipeDetail from "./pages/RecipeDetail";
+import Favorites from "./pages/Favorites";
+import SearchResults from "./pages/SearchResults";
+
+export default function App() {
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+       <Route path="/category/:categoryName" element={<Category />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/search" element={<SearchResults />} />
+    </Routes>
+    </Router>
+  );
 }
-
-export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { FavoritesProvider } from "./context/FavoritesContext";
+import "./index.css";
+import "./global.css";
+import "./app.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <FavoritesProvider>
+      <App />
+    </FavoritesProvider>
+  </React.StrictMode>
+);
